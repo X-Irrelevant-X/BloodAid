75d23cb46d9e11dba4cc35ec2d1f82aa37a3b449
X-Irrelevant-X <fardous.nayeem@g.bracu.ac.bd>
2025-04-25 05:12:45 +0600

    I cry

--- BA/encryption.py
+++ BA/encryption.py
@@ -1,5 +1,4 @@
 from cryptography.fernet import Fernet
-from werkzeug.security import *
 import os
 
 
@@ -20,11 +19,3 @@ def decrypt_data(data):
         return fernet.decrypt(data.encode()).decode()
     except:
         return data  # Return original if decryption fails
-
-def hash_password(password):
-    """Generate a secure password hash"""
-    return generate_password_hash(password)
-
-def verify_password(hashed_password, password):
-    """Verify a password against its hash"""
-    return check_password_hash(hashed_password, password)
